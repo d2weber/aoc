90a302fed207751d94d5d0dc8167d96e53ded7b4
d2weber <29163905+d2weber@users.noreply.github.com>
2023-04-02 12:21:08 +0200

    Complete d2.2

--- src/d2/mod.rs
+++ src/d2/mod.rs
@@ -1,12 +1,41 @@
-use std::cmp::Ordering;
-use std::str::FromStr;
 use Action::*;
+use Outcome::*;
 
-#[derive(PartialEq)]
+#[derive(Clone, PartialEq)]
 enum Action {
     Rock,
     Paper,
     Scissors,
+}
+
+#[derive(PartialEq)]
+enum Outcome {
+    Win,
+    Draw,
+    Lose,
+}
+
+impl Outcome {
+    fn score(&self) -> i32 {
+        match self {
+            Win => 6,
+            Draw => 3,
+            Lose => 0,
+        }
+    }
+}
+
+impl TryFrom<&str> for Outcome {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "X" => Ok(Lose),
+            "Y" => Ok(Draw),
+            "Z" => Ok(Win),
+            _ => Err("Invalid token to parse outcome"),
+        }
+    }
 }
 
 impl TryFrom<&str> for Action {
@@ -14,28 +43,15 @@ impl TryFrom<&str> for Action {
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         match value {
-            "A" | "X" => Ok(Action::Rock),
-            "B" | "Y" => Ok(Action::Paper),
-            "C" | "Z" => Ok(Action::Scissors),
+            "A" | "X" => Ok(Rock),
+            "B" | "Y" => Ok(Paper),
+            "C" | "Z" => Ok(Scissors),
             _ => Err("Invalid token to parse action"),
         }
     }
 }
 
 impl Action {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        use std::cmp::Ordering::*;
-        match (self, other) {
-            (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => Equal,
-            (Rock, Paper) => Less,
-            (Rock, Scissors) => Greater,
-            (Paper, Rock) => Greater,
-            (Paper, Scissors) => Less,
-            (Scissors, Rock) => Less,
-            (Scissors, Paper) => Greater,
-        }
-    }
-
     fn score(&self) -> i32 {
         match self {
             Rock => 1,
@@ -46,41 +62,119 @@ impl Action {
 }
 
 struct Match {
-    opponent: Action,
     own: Action,
+    other: Action,
+    outcome: Outcome,
 }
 
-impl FromStr for Match {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (opponent, own) = s.split_once(" ").ok_or("Couldn't split actions")?;
-        let opponent: Action = opponent.try_into()?;
-        let own: Action = own.try_into()?;
-        Ok(Match { opponent, own })
-    }
-}
-
+const GAME_LOGIC: [Match; 6] = [
+    Match {
+        own: Rock,
+        other: Paper,
+        outcome: Lose,
+    },
+    Match {
+        own: Rock,
+        other: Scissors,
+        outcome: Win,
+    },
+    Match {
+        own: Paper,
+        other: Rock,
+        outcome: Win,
+    },
+    Match {
+        own: Paper,
+        other: Scissors,
+        outcome: Lose,
+    },
+    Match {
+        own: Scissors,
+        other: Rock,
+        outcome: Lose,
+    },
+    Match {
+        own: Scissors,
+        other: Paper,
+        outcome: Win,
+    },
+];
 impl Match {
+    fn new(other: Action, own: Action) -> Self {
+        if own == other {
+            Match {
+                own,
+                other,
+                outcome: Draw,
+            }
+        } else {
+            GAME_LOGIC
+                .into_iter()
+                .find(|m| m.own == own && m.other == other)
+                .unwrap()
+        }
+    }
+    fn from_other_action(other: Action, outcome: Outcome) -> Self {
+        if matches!(outcome, Draw) {
+            Match {
+                own: other.clone(),
+                other,
+                outcome,
+            }
+        } else {
+            GAME_LOGIC
+                .into_iter()
+                .find(|m| m.other == other && m.outcome == outcome)
+                .unwrap()
+        }
+    }
+
     fn get_score(&self) -> i32 {
-        let outcome = match self.own.cmp(&self.opponent) {
-            Ordering::Less => 0,
-            Ordering::Equal => 3,
-            Ordering::Greater => 6,
-        };
-        outcome + self.own.score()
+        self.outcome.score() + self.own.score()
     }
 }
 
-mod part1 {
+pub mod part2 {
     use super::*;
+
     pub fn solution(input: &'static str) -> i32 {
         input
             .lines()
-            .map(Match::from_str)
-            .map(Result::unwrap)
-            .map(|m| m.get_score())
-            .sum::<i32>()
+            .map(|line| {
+                let (other, outcome) = line.split_once(' ').unwrap();
+                let other: Action = other.try_into().unwrap();
+                let outcome: Outcome = outcome.try_into().unwrap();
+                Match::from_other_action(other, outcome).get_score()
+            })
+            .sum()
+    }
+
+    #[test]
+    fn sample() {
+        let sample_input = "A Y
+B X
+C Z";
+        assert_eq!(solution(sample_input), 12);
+    }
+    #[test]
+    fn actual() {
+        assert_eq!(solution(include_str!("input")), 14184);
+    }
+}
+
+pub mod part1 {
+    use super::*;
+
+    pub fn solution(input: &'static str) -> i32 {
+        input
+            .lines()
+            .map(|line| {
+                let (other, own) = line.split_once(' ').unwrap();
+                let other: Action = other.try_into().unwrap();
+                let own: Action = own.try_into().unwrap();
+                Match::new(other, own).get_score()
+            })
+            .sum()
     }
 
     #[test]
@@ -95,4 +189,3 @@ C Z";
         assert_eq!(solution(include_str!("input")), 13675);
     }
 }
-pub use part1::solution;
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
+#![warn(clippy::all)]
+#![warn(
+    future_incompatible,
+    let_underscore,
+    nonstandard_style,
+    future_incompatible
+)]
+
 pub mod d1;
 pub mod d2;
