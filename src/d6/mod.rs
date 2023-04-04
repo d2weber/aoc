49c1b911be67fa0efaa7e83e487088c5b34f3946
d2weber <29163905+d2weber@users.noreply.github.com>
2023-04-04 12:40:19 +0200

    Complete d6

---
+++ src/d6/mod.rs
@@ -0,0 +1,68 @@
+#![cfg(test)]
+
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+fn interleaved_chunks<const N: usize, T>(
+    it: impl Iterator<Item = T>,
+) -> impl Iterator<Item = [T; N]>
+where
+    [T; N]: for<'a> TryFrom<&'a mut [T]>,
+{
+    let mut buffer = VecDeque::new();
+    it.filter_map(move |v| {
+        if buffer.len() > N - 1 {
+            buffer.pop_front();
+        }
+        buffer.push_back(v);
+        buffer.make_contiguous().try_into().ok()
+    })
+}
+
+fn offset<const N: usize>(input: &str) -> usize {
+    assert_eq!(input.len(), input.bytes().len());
+    interleaved_chunks::<N, _>(input.bytes())
+        .zip(N..)
+        .find_map(|(a, i)| {
+            let mut set = HashSet::new();
+            if a.iter().all(move |v| set.insert(v)) {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .unwrap()
+}
+
+mod part1 {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(offset::<4>("vwbjplbgvbhsrlpgdmjqwftvncz"), 4);
+        assert_eq!(offset::<4>("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(offset::<4>("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(offset::<4>("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(offset::<4>("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+    #[test]
+    fn actual() {
+        assert_eq!(offset::<4>(include_str!("input")), 1655);
+    }
+}
+mod part2 {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        assert_eq!(offset::<14>("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(offset::<14>("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(offset::<14>("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(offset::<14>("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(offset::<14>("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
+    }
+    #[test]
+    fn actual() {
+        assert_eq!(offset::<14>(include_str!("input")), 2665);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,4 @@ pub mod d2;
 pub mod d3;
 pub mod d4;
 pub mod d5;
+pub mod d6;
