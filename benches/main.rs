1363402f1c036b49788d53ecfc05373c24980668
d2weber <29163905+d2weber@users.noreply.github.com>
2023-04-07 14:20:17 +0200

    Rename dx to d0x

--- benches/main.rs
+++ benches/main.rs
@@ -1,15 +1,15 @@
 use aoc::*;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn d8(c: &mut Criterion) {
-    use d8::{part2::solution, INPUT};
-    c.bench_function("d8", |b| b.iter(|| solution(black_box(INPUT))));
+fn d08(c: &mut Criterion) {
+    use d08::{part2::solution, INPUT};
+    c.bench_function("d08", |b| b.iter(|| solution(black_box(INPUT))));
 }
 
-fn d9(c: &mut Criterion) {
-    use d9::{part2::solution, INPUT};
-    c.bench_function("d9", |b| b.iter(|| solution(black_box(INPUT))));
+fn d09(c: &mut Criterion) {
+    use d09::{part2::solution, INPUT};
+    c.bench_function("d09", |b| b.iter(|| solution(black_box(INPUT))));
 }
 
-criterion_group!(benches, d8, d9);
+criterion_group!(benches, d08, d09);
 criterion_main!(benches);
