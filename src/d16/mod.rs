3544e1f9cfe1dcf207097bb540a1eec9c3d57756
d2weber <29163905+d2weber@users.noreply.github.com>
2023-04-13 14:53:05 +0200

    d16: some cleanup

--- src/d16/mod.rs
+++ src/d16/mod.rs
@@ -1,24 +1,70 @@
 use itertools::Itertools;
-use std::{collections::HashMap, fmt::Debug};
+use std::{
+    collections::HashMap,
+    fmt::Debug,
+    ops::{Deref, DerefMut},
+};
 
 pub const SAMPLE: &str = include_str!("sample");
 pub const INPUT: &str = include_str!("input");
 
-type Id = [u8; 2];
+#[derive(Eq, Hash, PartialEq, Clone, Copy)]
+struct Id([u8; 2]);
 
-type DirectConnections = HashMap<Id, Vec<Id>>;
+impl From<&[u8]> for Id {
+    fn from(value: &[u8]) -> Self {
+        Id(value.try_into().unwrap())
+    }
+}
 
-#[derive(Clone, Hash, Debug)]
-struct Valve {
-    id: Id,
-    flow_rate: u32,
-    // distances: HashMap<ValveId, u32>
+impl Debug for Id {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(std::str::from_utf8(&self.0).unwrap())
+    }
+}
+
+impl Deref for Id {
+    type Target = [u8; 2];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Id {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
 }
 
 #[derive(Debug)]
-struct Distances {
-    d: HashMap<Id, HashMap<Id, u32>>,
-    direct_connections: DirectConnections,
+struct ValveMap(HashMap<Id, Valve>);
+
+impl FromIterator<(Id, Valve)> for ValveMap {
+    fn from_iter<T: IntoIterator<Item = (Id, Valve)>>(iter: T) -> Self {
+        Self(HashMap::from_iter(iter))
+    }
+}
+
+impl Deref for ValveMap {
+    type Target = HashMap<Id, Valve>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for ValveMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct Valve {
+    flow_rate: u32,
+    distances: HashMap<Id, u32>,
+    direct_connections: Vec<Id>,
 }
 
 #[derive(Debug)]
@@ -28,62 +74,35 @@ struct StepArgs {
     distance: u32,
 }
 
-impl Distances {
-    fn step(
-        &mut self,
-        StepArgs {
-            start,
-            current,
-            distance,
-        }: StepArgs,
-    ) -> Vec<StepArgs> {
-        let ds = &mut self.d.get_mut(&start).unwrap();
-        if *ds.get(&current).unwrap_or(&u32::MAX) > distance {
-            ds.insert(current, distance);
-            self.direct_connections[&current]
-                .iter()
-                .map(|id| StepArgs {
-                    start,
-                    current: *id,
-                    distance: distance + 1,
-                })
-                .collect()
-        } else {
-            vec![]
-        }
-    }
-
-    fn new<'a>(
-        direct_connections: DirectConnections,
-        ids: impl Iterator<Item = &'a Id> + 'a,
-    ) -> Distances {
-        let mut args: Vec<StepArgs> = direct_connections
+fn step(
+    StepArgs {
+        start,
+        current,
+        distance,
+    }: StepArgs,
+    valves: &mut ValveMap,
+) -> Vec<StepArgs> {
+    let ds = &mut valves.get_mut(&start).unwrap().distances;
+    if *ds.get(&current).unwrap_or(&u32::MAX) > distance {
+        ds.insert(current, distance);
+        let r = valves[&current]
+            .direct_connections
             .iter()
-            .flat_map(|(start, conns)| {
-                conns.iter().map(|dst| StepArgs {
-                    start: *start,
-                    current: *dst,
-                    distance: 1,
-                })
+            .filter(|id| **id != start)
+            .map(|id| StepArgs {
+                start,
+                current: *id,
+                distance: distance + 1,
             })
             .collect();
-        let mut distances = Distances {
-            d: direct_connections
-                .keys()
-                .map(|k| (*k, HashMap::new()))
-                .collect(),
-            direct_connections,
-        };
-        while !args.is_empty() {
-            args = args.into_iter().flat_map(|a| distances.step(a)).collect();
-        }
-        // dbg!(&distances);
-        distances
+        r
+    } else {
+        vec![]
     }
 }
 
-fn parse(s: &str) -> (Vec<Valve>, Distances) {
-    let (mut valves, direct_connections): (Vec<Valve>, HashMap<Id, Vec<Id>>) = s
+fn parse(s: &str) -> ValveMap {
+    let mut valves: ValveMap = s
         .lines()
         .map(|l| {
             let (id, l) = l
@@ -91,82 +110,97 @@ fn parse(s: &str) -> (Vec<Valve>, Distances) {
                 .unwrap()
                 .split_once(" has flow rate=")
                 .unwrap();
-            let id: Id = id.as_bytes().try_into().unwrap();
+
+            let id: Id = id.as_bytes().into();
             let (flow_rate, l) = l
                 .split_once("; tunnels lead to valves ")
                 .unwrap_or_else(|| l.split_once("; tunnel leads to valve ").unwrap());
-            let valve = Valve {
+            (
                 id,
-                flow_rate: flow_rate.parse().unwrap(),
-            };
-            let connections = l
-                .split(", ")
-                .map(|v| v.as_bytes().try_into().unwrap())
-                .collect();
-            (valve, (id, connections))
+                Valve {
+                    flow_rate: flow_rate.parse().unwrap(),
+                    direct_connections: l.split(", ").map(|v| v.as_bytes().into()).collect(),
+                    distances: HashMap::new(),
+                },
+            )
         })
-        .unzip();
-    valves.retain(|v| v.flow_rate > 0);
-    let distances = Distances::new(direct_connections, valves.iter().map(|v| &v.id));
-    (valves, distances)
+        .collect();
+    let mut args: Vec<StepArgs> = valves
+        .iter()
+        .flat_map(
+            |(
+                id,
+                Valve {
+                    direct_connections, ..
+                },
+            )| {
+                direct_connections.iter().map(|dst| StepArgs {
+                    start: *id,
+                    current: *dst,
+                    distance: 1,
+                })
+            },
+        )
+        .collect();
+    for _ in 0..100 {
+        if args.is_empty() {
+            return valves;
+        }
+        args = args
+            .into_iter()
+            .flat_map(|a| step(a, &mut valves))
+            .collect();
+    }
+    panic!("Too many steps");
+    // distances.valves
 }
 
 const N_STEPS: u32 = 30;
+const START_ID: Id = Id([b'A', b'A']);
 
 pub mod part1 {
     use super::*;
 
-    pub fn slow_solution(s: &str) -> u32 {
-        let (valves, distances) = parse(s);
-        let k = valves.len();
-        valves
-            .into_iter()
-            .permutations(k)
-            .map(|x| {
-                // dbg! {&x};
-                let mut last_valve = [b'A', b'A'];
-                x.iter()
-                    .scan(N_STEPS, |steps_left, valve| {
-                        if let Some(next_steps_left) = steps_left.checked_sub(
-                            distances.d[&last_valve][&valve.id] /* time to walk */
-                         + 1, /* time to open the valve */
-                        ) {
-                            *steps_left = next_steps_left;
-                        } else {
-                            return None;
-                        };
-                        last_valve = valve.id;
-                        // dbg!(valve.flow_rate, &steps_left);
-                        Some(valve.flow_rate * (*steps_left))
-                    })
-                    .sum()
-            })
-            // .count() as u32
-            .max()
-            .unwrap()
-    }
     pub fn solution(s: &str) -> u32 {
-        let (valves, distances) = parse(s);
-        // let mut vs = vec!["AA".to_owned()];
-        // let mut unused = valves;
+        let mut valves = parse(s);
+        valves.retain(|k, v| *k == START_ID || v.flow_rate > 0);
         let k = valves.len();
         valves
-            .into_iter()
-            .permutations(k)
+            .iter()
+            .filter(|(k, _)| **k != START_ID)
+            .permutations(k - 1)
+            .map(|mut x| {
+                let mut last_k = None;
+                let mut total = 0;
+                x.retain(|(k, v)| {
+                    if total > N_STEPS {
+                        false
+                    } else {
+                        if let Some(lk) = last_k {
+                            total += v.distances[lk];
+                        }
+                        last_k = Some(k);
+                        true
+                    }
+                });
+                x
+            })
+            .dedup()
             .map(|x| {
                 // dbg! {&x};
-                let mut last_valve = [b'A', b'A'];
+                let mut last_valve = Id([b'A', b'A']);
                 x.iter()
-                    .scan(N_STEPS, |steps_left, valve| {
+                    .scan(N_STEPS, |steps_left, (id, valve)| {
+                        // dbg!(last_valve, id);
                         if let Some(next_steps_left) = steps_left.checked_sub(
-                            distances.d[&last_valve][&valve.id] /* time to walk */
+                            valves[&last_valve].distances[*id] /* time to walk */
                      + 1, /* time to open the valve */
                         ) {
                             *steps_left = next_steps_left;
                         } else {
                             return None;
                         };
-                        last_valve = valve.id;
+                        last_valve = **id;
                         // dbg!(valve.flow_rate, &steps_left);
                         Some(valve.flow_rate * (*steps_left))
                     })
