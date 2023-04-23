ae3a8c61211353a5a5e97137b2d33c71dd5e19a2
d2weber <29163905+d2weber@users.noreply.github.com>
2023-04-23 08:25:46 +0200

    Complete y2022:d03

--- src/y2021/d02/mod.rs
+++ src/y2021/d02/mod.rs
@@ -29,3 +29,34 @@ pub mod part1 {
         assert_eq!(solution(INPUT), 1427868);
     }
 }
+pub mod part2 {
+    #[cfg(test)]
+    use super::*;
+
+    pub fn solution(s: &str) -> i32 {
+        let mut pos = 0;
+        let mut depth = 0;
+        let mut aim = 0;
+        s.lines().for_each(|l| {
+            if let Some(v) = l.strip_prefix("up ") {
+                aim -= v.parse::<i32>().unwrap();
+            } else if let Some(v) = l.strip_prefix("down ") {
+                aim += v.parse::<i32>().unwrap();
+            } else {
+                let x = l.strip_prefix("forward ").unwrap().parse::<i32>().unwrap();
+                pos += x;
+                depth += x * aim;
+            }
+        });
+        pos * depth
+    }
+
+    #[test]
+    fn sample() {
+        assert_eq!(solution(SAMPLE), 900);
+    }
+    #[test]
+    fn actual() {
+        assert_eq!(solution(INPUT), 1568138742);
+    }
+}
